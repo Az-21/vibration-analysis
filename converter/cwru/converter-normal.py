4d63e2ab25487a3616679b813aa7cea8cd21f58c
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-03-11 22:17:24 +0530

    Run `ruff` format

--- converter/cwru/converter-normal.py
+++ converter/cwru/converter-normal.py
@@ -7,73 +7,73 @@ from utility import flatten, find_mat_files
 
 
 def convert_mat_to_csv(filepath):
-    # Get filename from full path
-    filename = os.path.basename(filepath)
-    filename, extension = os.path.splitext(filename)  # Strip extension from filename | Discard extension
+  # Get filename from full path
+  filename = os.path.basename(filepath)
+  filename, extension = os.path.splitext(filename)  # Strip extension from filename | Discard extension
 
-    match_hp = re.search(r"(\d+)(hp)", filename)
-    if not match_hp:
-        print("Could not match motor HP.\nTerminating program to prevent error...")
+  match_hp = re.search(r"(\d+)(hp)", filename)
+  if not match_hp:
+    print("Could not match motor HP.\nTerminating program to prevent error...")
+    sys.exit()
+  metadata_hp = int(match_hp.group(1))
+
+  # Load MATLAB file
+  data = loadmat(f"input/{filename}.mat")
+
+  # Select the unique column ID | example X123RPM => 123
+  id = 0
+  for key in data.keys():
+    if "RPM" in key:
+      match_id = re.search(r"\d+", key)
+      if not match_id:
+        print("Could not match column ID.\nTerminating program to prevent error...")
         sys.exit()
-    metadata_hp = int(match_hp.group(1))
+      id = match_id.group()
 
-    # Load MATLAB file
-    data = loadmat(f"input/{filename}.mat")
+  # Special case (incorrectly formatted `normal-1hp` and `normal-2hp` from Case Western dataset)
+  special_case_flag = False
+  metadata_rpm = 0
+  if id == 0:
+    special_case_flag = True
+    if filename == "normal-1hp":
+      id = "098"
+      metadata_rpm = 1772
+    elif filename == "normal-2hp":
+      id = "099"
+      metadata_rpm = 1750
+    else:
+      print("Filename does not match any special case files.\nTerminating program to prevent error...")
+      sys.exit()
 
-    # Select the unique column ID | example X123RPM => 123
-    id = 0
-    for key in data.keys():
-        if "RPM" in key:
-            match_id = re.search(r"\d+", key)
-            if not match_id:
-                print("Could not match column ID.\nTerminating program to prevent error...")
-                sys.exit()
-            id = match_id.group()
+  # Import columns
+  de = flatten(data[f"X{id}_DE_time"])
+  fe = flatten(data[f"X{id}_FE_time"])
+  if special_case_flag == False:
+    metadata_rpm = data[f"X{id}RPM"][0][0]
 
-    # Special case (incorrectly formatted `normal-1hp` and `normal-2hp` from Case Western dataset)
-    special_case_flag = False
-    metadata_rpm = 0
-    if id == 0:
-        special_case_flag = True
-        if filename == "normal-1hp":
-            id = "098"
-            metadata_rpm = 1772
-        elif filename == "normal-2hp":
-            id = "099"
-            metadata_rpm = 1750
-        else:
-            print("Filename does not match any special case files.\nTerminating program to prevent error...")
-            sys.exit()
+  # Append metadata
+  length = len(de)
+  hp = [metadata_hp] * length
+  rpm = [metadata_rpm] * length
 
-    # Import columns
-    de = flatten(data[f"X{id}_DE_time"])
-    fe = flatten(data[f"X{id}_FE_time"])
-    if special_case_flag == False:
-        metadata_rpm = data[f"X{id}RPM"][0][0]
+  # Create table
+  df = pd.DataFrame(
+    {
+      "HP": hp,
+      "RPM": rpm,
+      "DE": de,
+      "FE": fe,
+    }
+  )
 
-    # Append metadata
-    length = len(de)
-    hp = [metadata_hp] * length
-    rpm = [metadata_rpm] * length
-
-    # Create table
-    df = pd.DataFrame(
-        {
-            "HP": hp,
-            "RPM": rpm,
-            "DE": de,
-            "FE": fe,
-        }
-    )
-
-    # Preview and save
-    print(df.head())
-    if not os.path.exists("output/"):
-        os.makedirs("output/")
-    df.to_csv(f"output/{filename}.csv", index=False)
+  # Preview and save
+  print(df.head())
+  if not os.path.exists("output/"):
+    os.makedirs("output/")
+  df.to_csv(f"output/{filename}.csv", index=False)
 
 
 # Run script
 mat_files = find_mat_files("input/")
 for mat_file in mat_files:
-    convert_mat_to_csv(mat_file)
+  convert_mat_to_csv(mat_file)
--- converter/cwru/utility.py
+++ converter/cwru/utility.py
@@ -2,13 +2,13 @@ import os
 
 
 def flatten(xss):
-    return [x for xs in xss for x in xs]
+  return [x for xs in xss for x in xs]
 
 
 def find_mat_files(directory):
-    mat_files = []
-    for root, _, files in os.walk(directory):
-        for filename in files:
-            if filename.endswith(".mat"):
-                mat_files.append(os.path.join(root, filename))
-    return mat_files
+  mat_files = []
+  for root, _, files in os.walk(directory):
+    for filename in files:
+      if filename.endswith(".mat"):
+        mat_files.append(os.path.join(root, filename))
+  return mat_files
