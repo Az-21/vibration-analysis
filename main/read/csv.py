01afd8f123bab5dd7e02a35cb5320dc2ebfb9964
Az-21 <39803478+Az-21@users.noreply.github.com>
2024-03-25 10:30:33 +0530

    Rename function to better match utility
    In context of import as main.read.csv.as_list()

--- main/read/csv.py
+++ main/read/csv.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 
-def fetch_csv(directory):
+def as_list(directory):
   dfs = []
 
   # Iterate over each item in the directory
@@ -11,7 +11,7 @@ def fetch_csv(directory):
 
     if os.path.isdir(item_path):
       # If the item is a directory, recursively call the function
-      dfs.extend(fetch_csv(item_path))
+      dfs.extend(as_list(item_path))
     elif item.endswith(".csv"):
       # If the item is a CSV file, read it into a DataFrame and append to the list
       df = pd.read_csv(item_path)
